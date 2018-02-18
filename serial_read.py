77ac9c1d3f19c6d38551827f500b7c44ecae585f
dschurman <dschurman1@gmail.com>
2018-02-18 07:11:37 -0800

    fixed query string

--- serial_read.py
+++ serial_read.py
@@ -5,8 +5,10 @@ import requests
 def serial_in(resp_port, ard_port):
 	ser_resp = serial.Serial(resp_port, 9600, timeout=2)
 	ser_ard = serial.Serial(ard_port, 9600, timeout=2)
+	time.sleep(1)
 	ser_resp.write(b'c')
-	print(ser_resp.readline(), '***')
+	time.sleep(1)
+	#print(ser_resp.readline(), '***')
 	while True:
 		line_resp = ser_resp.readline()
 		line_ard = ser_ard.readline()
@@ -15,7 +17,7 @@ def serial_in(resp_port, ard_port):
 		query_resp = line_to_query(str(line_resp, 'utf-8'), cur_time)
 		tok_ard = line_ard.split(' ')
 		if tok_ard[0] == '':
-			query_resp += '&carbonmonoxide=null&co2=null'
+			query_resp += '&carbonmonoxide=-1&co2=-1'
 		else:
 			query_resp += '&carbonmonoxide=' + tok_ard[0] + '&co2=' + tok_ard[1]
 		final_str = "https://air-quality-195519.appspot.com/insert" + query_resp + '&location=0,0'
@@ -30,7 +32,7 @@ def line_to_query(line, read_time):
 	# '110816030320, 107, 25, 21, 32598, 26884, 14454, 00, 00, 01, 49'	
 	print(line == '')
 	if line == '':
-		return '?respiratoryirritants=null&datetime=' + str(read_time)
+		return '?respiratoryirritants=-1&datetime=' + str(read_time)
 	tok = line.split(', ')
 	'''
 	dict = {'PPB' : 0, 'TEMP' : 0, 'RH' : 0, 'RawSensor' : 0, 'TempDigital' : 0, 'RHDigital' : 0, 'SecTime' : 0, 'DateTime' : ''}
